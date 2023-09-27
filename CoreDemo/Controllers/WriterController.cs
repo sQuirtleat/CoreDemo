0d77b4821a78227c9dd07dd3d865945723a9a432
sQuirtleat <bilgicselim32@gmail.com>
2023-09-28 00:54:43 +0300

    Notification sınıfı

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 namespace CoreDemo.Controllers
 {
 	[AllowAnonymous]
 	public class WriterController : Controller
 	{
-
+		WriterManager wm=new WriterManager(new EfWriterRepository());
 		public IActionResult Index()
 		{
 			return View();
@@ -24,6 +32,65 @@ namespace CoreDemo.Controllers
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+		[HttpGet]
+		public IActionResult WriterEditProfile()
+		{
+			var writervalues =wm.TGetById(1);
+			return View(writervalues);
+		}
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p,string confirmPassword)
+        {
+			if (p.WriterPassword == confirmPassword)
+			{
+				WriterValidator wl = new WriterValidator();
+				ValidationResult results = wl.Validate(p);
+				if (results.IsValid)
+				{
+					wm.TUpdate(p);
+					return RedirectToAction("Index", "Dashboard");
+				}
+				else
+				{
+					foreach (var item in results.Errors)
+					{
+						ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+					}
+				}
+			}
+            else
+            {
+                ModelState.AddModelError("ConfirmPassword", "Şifreler eşleşmiyor.");
+            }
+
+            return View();
+        }
+		[HttpGet]
+		public IActionResult WriterAdd()
+		{
+			return View();
+		}
+		[HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+			Writer w = new Writer();
+			if(p.WriterImage != null)
+			{
+				var extension = Path.GetExtension(p.WriterImage.FileName);
+				var newimagename=Guid.NewGuid()+ extension;
+				var location=Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+				var stream=new FileStream(location, FileMode.Create);
+				p.WriterImage.CopyTo(stream);
+				w.WriterImage = newimagename;
+			}
+			w.WriterMail = p.WriterMail;
+			w.WriterName = p.WriterName;
+			w.WriterPassword = p.WriterPassword;
+			w.WriterStatus = p.WriterStatus;
+			w.WriterAbout = p.WriterAbout;
+			wm.TAdd(w);
+			return RedirectToAction("Index", "Dashboard");
         }
     }  
 }
---
+++ CoreDemo/Models/AddProfileImage.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace CoreDemo.Models
+{
+    public class AddProfileImage
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterAbout { get; set; }
+        public IFormFile WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+        public bool WriterStatus { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
